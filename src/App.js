21e1edd9a70ca7781fd1767c3c61f271f44bb4dc
gael <gael.blanchard@epitech.eu>
2021-08-31 12:30:06 +0200

    form addPage

--- src/App.js
+++ src/App.js
@@ -1,62 +1,98 @@
 import React from "react";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, Link, Route, Switch} from "react-router-dom";
 import "./Css/App.css";
-import {Box, Container, Link, makeStyles, TextField} from "@material-ui/core";
+import {Box, Container, makeStyles, TextField} from "@material-ui/core";
 import ViewModuleIcon from '@material-ui/icons/ViewModule';
 import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
 import ImportContactsIcon from '@material-ui/icons/ImportContacts';
+import Module from "./View/ModuleFormation";
 
 
-const styles = theme => ({
-  multilineColor:{
-    color:'red'
-  }
+const useStyles = makeStyles({
+    input: {
+        backgroundColor: "skyblue",
+        borderColor: "white !important",
+        color: "white"
+    },
+    label: {
+        color: "white",
+    },
+
+   focused:{
+        borderColor:"white"
+   }
+
 });
 
 
 function App() {
-  return (
-<div>
+    const classes = useStyles();
+
+    return (
+        <div>
 
 
-  <BrowserRouter>
+            <BrowserRouter>
+
+                <div className="navbar">
+
+                    <Container style={{display: "flex", marginLeft: "10px", flexWrap: "wrap"}}>
+                        <Box className="boxNavbar">
+                            <ViewModuleIcon style={{color: "white"}}/>
+                            <Link to="/modules" className="linkNavbar" >Modules</Link>
+                        </Box>
+
+                        <Box className="boxNavbar">
+                            <SupervisorAccountIcon style={{color: "white"}}/>
+                            <Link to="/addModules" className="linkNavbar" >Administration</Link>
+                        </Box>
+
+                    </Container>
+
+                    <Container style={{display: "flex", marginLeft: "10px", flexWrap: "wrap"}}>
+
+                        <Box className="boxNavbar">
+                            {/*<ImportContactsIcon style={{color: "white"}}/>*/}
+                            {/*<form>*/}
+                            {/*    <TextField*/}
+                            {/*        InputLabelProps={{*/}
+                            {/*            className: classes.label,*/}
+                            {/*            classes: {*/}
+                            {/*                focused: classes.focused*/}
+                            {/*            }*/}
+                            {/*        }}*/}
+
+                            {/*        InputProps={{*/}
+                            {/*            className: classes.input,*/}
+                            {/*            classes: {*/}
+                            {/*                focused: classes.focused,*/}
+                            {/*                rout: classes.outlined*/}
+                            {/*            }*/}
+                            {/*        }}*/}
+                            {/*        size="small"*/}
+                            {/*        label="Outlined"*/}
+                            {/*        variant="outlined"*/}
+                            {/*    />*/}
+                            {/*</form>*/}
+                        </Box>
+
+                    </Container>
+
+                </div>
+
+                <Switch>
+                    <Route exact path = "/addModules">
+                        <Module/>
+                    </Route>
+                </Switch>
+
+            </BrowserRouter>
 
 
-    <div className="navbar">
-
-        <Container style={{display:"flex", marginLeft:"10px", flexWrap:"wrap"}}>
-
-          <Box className="boxNavbar">
-            <ViewModuleIcon style={{color:"white"}}/>
-            <Link className="linkNavbar" component="button" href="">Modules</Link>
-          </Box>
-
-          <Box className="boxNavbar">
-            <SupervisorAccountIcon style={{color: "white"}}/>
-            <Link className="linkNavbar" component="button" href="">Administration</Link>
-          </Box>
-
-        </Container>
-
-      <Container style={{display:"flex", marginLeft:"10px", flexWrap:"wrap"}}>
-
-        <Box className="boxNavbar">
-          <ImportContactsIcon style={{color:"white"}}/>
-          <form>
-            <TextField  InputLabelProps={{className:styles.multilineColor}} size="small" label="Outlined" variant="outlined" />
-          </form>
-        </Box>
-
-      </Container>
-
-    </div>
 
 
-  </BrowserRouter>
-
-
-</div>
-  );
+        </div>
+    );
 }
 
 export default App;
--- src/View/ModuleFormation.js
+++ src/View/ModuleFormation.js
@@ -1,13 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
 import Paper from '@material-ui/core/Paper';
+import CssAddModule from "../Css/AddModule.module.css"
+import { Editor } from "react-draft-wysiwyg";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import {Box, Button, Container} from "@material-ui/core";
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import CameraIcon from '@material-ui/icons/Camera';
+
 
 function Module() {
+    const [editorState, setEditorState] = useState("")
+    console.log(editorState);
+
+
     return (<>
 
 
-        <Paper elevation={3}>
-            test
-        </Paper>
+<Container className={CssAddModule.containerText}>
+    <Box className={CssAddModule.title}>
+        <CameraIcon />
+        <h2>Ajouter une page au module</h2>
+    </Box>
+    <Paper elevation={3} className={CssAddModule.paperAddModule}>
+        <Button
+            variant="contained"
+            color="default"
+            className={CssAddModule.buttonVideo}
+            startIcon={<CloudUploadIcon />}
+        >
+            Upload
+        </Button>
+
+        <Editor
+            editorState={editorState}
+            onEditorStateChange={setEditorState}
+            wrapperClassName="wrapper-class"
+            editorClassName="editor-class"
+            toolbarClassName="toolbar-class"
+        />
+
+    </Paper>
+</Container>
+
 
     </>);
 }
